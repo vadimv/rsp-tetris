4cd0eafcc76145d49b011fd075b78796ea8527e5
Vadim Vashkevich <vadim.vashkevich@gmail.com>
2023-07-12 01:56:52 +0200

    Upgrade to rsp 1.0

--- src/main/java/rsp/tetris/Tetris.java
+++ src/main/java/rsp/tetris/Tetris.java
@@ -1,10 +1,10 @@
 package rsp.tetris;
 
 import rsp.App;
-import rsp.Component;
 import rsp.jetty.JettyServer;
 import rsp.ref.TimerRef;
 import rsp.server.StaticResources;
+import rsp.stateview.ComponentView;
 
 import java.io.File;
 import java.nio.CharBuffer;
@@ -31,63 +31,59 @@ public class Tetris {
     private static final TimerRef FALLING_ITEMS_TIMER = TimerRef.createTimerRef();
 
     public static void main(String[] args) {
-        final Component<State> component = useState ->
-            html(on("keydown", false, c -> {
-                        final String keyCode = c.eventObject().value("keyCode").map(Object::toString).orElse("noKeyCode");
-                        final State s = useState.get();
-                        switch (keyCode) {
-                            case LEFT_KEY  : s.tryMoveLeft().ifPresent(ns -> useState.accept(ns)); break;
-                            case RIGHT_KEY : s.tryMoveRight().ifPresent(ns -> useState.accept(ns)); break;
-                            case DOWN_KEY  : s.tryMoveDown().ifPresent(ns -> useState.accept(ns)); break;
-                            case UP_KEY    : s.tryRotate().ifPresent(ns -> useState.accept(ns)); break;
-                        }
-                    }),
-                head(title("Tetris"),
-                                       /*     link(attr("rel", "preconnect"), attr("href","https://fonts.googleapis.com")),
-                     link(attr("rel", "preconnect"), attr("href","https://fonts.gstatic.com"), attr("crossorigin")),
-                     link(attr("rel", "stylesheet"), attr("href","https://fonts.googleapis.com/css2?family=VT323&display=swap")),
-               */
-                     link(attr("rel", "stylesheet"), attr("href","/res/style.css")),
-                     meta(attr("http-equiv","Content-Type"), attr("content", "text/html; charset=UTF-8"))),
-                body(div(attr("id", "header")),
-                     div(attr("id", "content"),
-                        div(attr("class", "left-column")),
-                        div(attr("class", "tetris-wrapper"),
-                        div(attr("class", "stage"),
-                            of(Arrays.stream(useState.get().stage.cells()).flatMap(row ->
-                                    CharBuffer.wrap(row).chars().mapToObj(i -> (char)i)).map(cell ->
-                                        div(attr("class", "cell t" + cell))))),
-                        div(attr("class", "sidebar"),
-                            div(attr("id", "score"),
-                                p("SCORE"),
-                                span(attr("class", "score-text"),
-                                     text(String.format("%06d", useState.get().score())))),
-                            div(attr("id", "start"),
-                                button(attr("id", "start-btn"), attr("type", "button"),
-                                       when(useState.get().isRunning, () -> attr("disabled")),
-                                       text("Start"),
-                                       on("click", c -> {
-                                                      State.initialState().start().newTetramino().ifPresent(ns -> useState.accept(ns));
-                                                      c.scheduleAtFixedRate(() -> useState.acceptOptional(
-                                                              s -> s.tryMoveDown()
-                                                                    .or(() -> s.newTetramino())
-                                                                    .or(() -> {
-                                                                       c.cancelSchedule(FALLING_ITEMS_TIMER);
-                                                                       return Optional.of(s.stop());
-                                           })), FALLING_ITEMS_TIMER, 0, 1, TimeUnit.SECONDS);
-                                       })),
-                                p("← → ↓ move"),
-                                p("↑ rotate")))),
-                        div(attr("class", "right-column"))
-                ),
-                        div(attr("id", "footer"))));
+        final ComponentView<State> component = state -> newState ->
+                html(on("keydown", false, c -> {
+                            final String keyCode = c.eventObject().value("keyCode").map(Object::toString).orElse("noKeyCode");
+                            final State s = state;
+                            switch (keyCode) {
+                                case LEFT_KEY  : s.tryMoveLeft().ifPresent(ns -> newState.set(ns)); break;
+                                case RIGHT_KEY : s.tryMoveRight().ifPresent(ns -> newState.set(ns)); break;
+                                case DOWN_KEY  : s.tryMoveDown().ifPresent(ns -> newState.set(ns)); break;
+                                case UP_KEY    : s.tryRotate().ifPresent(ns -> newState.set(ns)); break;
+                            }
+                        }),
+                        head(title("Tetris"),
+                                link(attr("rel", "stylesheet"), attr("href","/res/style.css")),
+                                meta(attr("http-equiv","Content-Type"), attr("content", "text/html; charset=UTF-8"))),
+                        body(div(attr("id", "header")),
+                                div(attr("id", "content"),
+                                        div(attr("class", "left-column")),
+                                        div(attr("class", "tetris-wrapper"),
+                                                div(attr("class", "stage"),
+                                                        of(Arrays.stream(state.stage.cells()).flatMap(row ->
+                                                                CharBuffer.wrap(row).chars().mapToObj(i -> (char)i)).map(cell ->
+                                                                div(attr("class", "cell t" + cell))))),
+                                                div(attr("class", "sidebar"),
+                                                        div(attr("id", "score"),
+                                                                p("SCORE"),
+                                                                span(attr("class", "score-text"),
+                                                                        text(String.format("%06d", state.score())))),
+                                                        div(attr("id", "start"),
+                                                                button(attr("id", "start-btn"), attr("type", "button"),
+                                                                        when(state.isRunning, () -> attr("disabled")),
+                                                                        text("Start"),
+                                                                        on("click", c -> {
+                                                                            State.initialState().start().newTetramino().ifPresent(ns -> newState.set(ns));
+                                                                            c.scheduleAtFixedRate(() -> newState.applyIfPresent(
+                                                                                    s -> s.tryMoveDown()
+                                                                                            .or(() -> s.newTetramino())
+                                                                                            .or(() -> {
+                                                                                                c.cancelSchedule(FALLING_ITEMS_TIMER);
+                                                                                                return Optional.of(s.stop());
+                                                                                            })), FALLING_ITEMS_TIMER, 0, 1, TimeUnit.SECONDS);
+                                                                        })),
+                                                                p("← → ↓ move"),
+                                                                p("↑ rotate")))),
+                                        div(attr("class", "right-column"))
+                                ),
+                                div(attr("id", "footer"))));
 
-        final var s = new JettyServer(SERVER_PORT,
-                                     "",
-                                      new App(State.initialState(),
-                                              component),
-                                      new StaticResources(new File("src/main/java/rsp/tetris"),
-                                                                   "/res/*"));
+        final var s = new JettyServer<>(SERVER_PORT,
+                "",
+                new App<>(State.initialState(),
+                        component),
+                new StaticResources(new File("src/main/java/rsp/tetris"),
+                        "/res/*"));
         s.start();
         s.join();
     }
